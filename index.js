4c20224c93c7fe03141c953f04df21ab5a4e0d3c
Assami Muzaki <muzakiassami@gmail.com>
2023-03-12 22:08:49 +0700

    init sequelized mysql2

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ import morgan from "morgan";
 
 // ROUTER
 import connectDb from "./src/services/db.js";
-import authRouter from "./src/routes/authRoutres.js";
+// import authRouter from "./src/routes/authRoutres.js";
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
-app.use("/auth", authRouter);
+// app.use("/auth", authRouter);
 
-async function startServer() {
-  await connectDb();
+function startServer() {
+  connectDb();
   app.listen(3000, () => console.log("server run at port 3000"));
 }
 startServer();
