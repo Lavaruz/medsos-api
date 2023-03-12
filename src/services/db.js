5e97d3f19d6be624ca7243eaafc64685e51eabe8
Assami Muzaki <muzakiassami@gmail.com>
2023-03-12 21:26:40 +0700

    migrate to mysql

--- src/services/db.js
+++ src/services/db.js
@@ -1,9 +1,1 @@
-import mongoose from "mongoose";
-
-async function connectDb() {
-  await mongoose.connect("mongodb://localhost:27017/Medsos").then(() => {
-    console.log("connect to db");
-  });
-}
-
-export default connectDb;
+// USING MYSQL
